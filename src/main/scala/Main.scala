40daa814db8a996f557d91dc2358fede41f31844
Hendrik Ziegler <hendrikziegler@outlook.de>
2023-10-11 18:56:52 +0200

    ITERABLES WILL BE USED UNTIL MORALE IMPROVES

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,20 +3,34 @@ import scala.util.Random
 val rand = new Random()
 
 @main def main: Unit =
-  print(get_field(8, 8))
+	val width = 8
+	val height = 8
+	val bomb_chance = 0.25f
+	val bombs = gen_bombs(width, height, bomb_chance)
 
-def get_field(width: Int, height: Int): String = 
-	val hidden   = "X"
-	val revealed = "▯"
-	val bomb     = "B"
+	print(get_field(width, height, bombs))
 
-	def get_random_field(percent: Float): String = 
-		rand.nextInt((1/percent).toInt) match
-		case 0 => bomb
-		case _ => hidden
+// returns a string representation of the field
+def get_field(width: Int, height: Int, bombs: List[(Int, Int)]): String = 
+	// constants for the field
+	val hidden   = "#"
+	val revealed = " "
+	val bomb     = "o"
 
-	(0 until height).map(
-		_ => (0 until width).map(_ => get_random_field(0.25) + " ")
+	// generate the field as a string
+	(0 until height).map(y => 
+		(0 until width).map(x => 
+			(if bombs.contains((x, y)) 
+				then bomb 
+				else hidden
+			) + " ")
 		.toArray.mkString + "\n"
 	).toArray.mkString
 
+// generates a list of bomb coordinates with a given percentage
+def gen_bombs(width: Int, height: Int, percent: Float): List[(Int, Int)] =
+	(for {
+		x <- 0 until width
+		y <- 0 until height
+		if rand.nextInt((1/percent).toInt) == 0
+	} yield (x, y)).toList
