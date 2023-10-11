5279aa1f16e9cc0008870d3f5818255e890fbf8d
Hendrik Ziegler <hendrikziegler@outlook.de>
2023-10-11 18:35:38 +0200

    playing with strings and ranges

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import scala.util.Random
 
-def msg = "I was compiled by Scala 3. :)"
+val rand = new Random()
+
+@main def main: Unit =
+  print(get_field(8, 8))
+
+def get_field(width: Int, height: Int): String = 
+	val hidden   = "X"
+	val revealed = "▯"
+	val bomb     = "B"
+
+	def get_random_field(percent: Float): String = 
+		rand.nextInt((1/percent).toInt) match
+		case 0 => bomb
+		case _ => hidden
+
+	(0 until height).map(
+		_ => (0 until width).map(_ => get_random_field(0.25) + " ")
+		.toArray.mkString + "\n"
+	).toArray.mkString
+
