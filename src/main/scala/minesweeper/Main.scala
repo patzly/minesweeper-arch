c7e24b3ad057aa45cd08ef782e4ae02424697dd1
NotLe0n <notle0n75@gmail.com>
2023-10-27 09:25:26 +0200

    better Field matrix init

--- src/main/scala/minesweeper/Main.scala
+++ src/main/scala/minesweeper/Main.scala
@@ -9,7 +9,7 @@ import controller.genbombRand
 	val height = 8
 	val bomb_chance = 0.25f
 	val rand = new Random()
-	val field = Field(width, height, genbombRand(rand, bomb_chance))
+	val field: Field = Field(width, height, genbombRand(rand, bomb_chance))
 
 	print(field)
 
--- src/main/scala/minesweeper/model/Field.scala
+++ src/main/scala/minesweeper/model/Field.scala
@@ -3,9 +3,7 @@ package minesweeper.model
 import java.util.Random
 
 class Field(rows: Int, cols: Int, genbomb: (Int, Int) => Cell) {
-	val matrix = (0 until cols).map(y => 
-		(0 until rows).map(x => genbomb(x, y)).toVector
-	).toVector
+	val matrix = Vector.tabulate(rows, cols) {(x, y) => genbomb(x, y)}
 
 	override def toString(): String = {
 		matrix.map(r => r.mkString(" ")).mkString("\n")
