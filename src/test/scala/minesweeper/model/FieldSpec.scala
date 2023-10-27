b0a465f14a1407156707de9378b217e87788ae1d
NotLe0n <notle0n75@gmail.com>
2023-10-27 09:00:36 +0200

    added scoverage

--- src/test/scala/minesweeper/model/FieldSpec.scala
+++ src/test/scala/minesweeper/model/FieldSpec.scala
@@ -11,18 +11,28 @@ class FieldSpec extends AnyWordSpec {
 			val fieldRevealed = Field(1, 1, (x, y) => Cell(true, false))
 			val fieldBomb = Field(1, 1, (x, y) => Cell(true, true))
 
-			"have a single hidden Cell" in {
+			"have a single Cell" in {
+				fieldHidden.matrix.size shouldBe(1)
+				fieldHidden.matrix.head.size shouldBe(1)
+			}
+
+			"print a single hidden Cell" in {
 				fieldHidden.toString shouldBe("#")
 			}
-			"have a single revealed empty Cell" in {
+			"print a single revealed empty Cell" in {
 				fieldRevealed.toString shouldBe("☐")
 			}
-			"have a single revealed bomb Cell" in {
+			"print a single revealed bomb Cell" in {
 				fieldBomb.toString shouldBe("☒")
 			}
 		}
 		"it has 3 rows and columns and is empty" should {
 			val fieldRevealed = Field(3, 3, (x, y) => Cell(true, false))
+			"have a 3 columns and 3 rows" in {
+				fieldRevealed.matrix.size shouldBe(3)
+				fieldRevealed.matrix.head.size shouldBe(3)
+			}
+
 			"be printed correctly if all Cells are revealed" in {
 				fieldRevealed.toString shouldBe("☐ ☐ ☐\n☐ ☐ ☐\n☐ ☐ ☐")
 			}
