53220311b07588142a00cdbfcf52e58e8afd035b
Hendrik Ziegler <hendrikziegler@outlook.de>
2023-11-01 12:17:10 +0100

    TUI lost/won + tests

--- src/main/scala/minesweeper/view/Tui.scala
+++ src/main/scala/minesweeper/view/Tui.scala
@@ -3,33 +3,41 @@ package minesweeper.view
 import minesweeper.controller.FieldController
 
 class Tui(controller: FieldController) {
-    private def invalidInput(msg: String): Boolean = {
-        System.err.println(msg)
-        true
-    }
-    private def invalidInput(): Boolean = invalidInput("Invalid input")
+	private def invalidInput(msg: String): Boolean = {
+	    System.err.println(msg)
+	    true
+	}
+	private def invalidInput(): Boolean = invalidInput("Invalid input")
 
-    def processLine(line: String): Boolean = {
-        line match {
-            case "q" => return false
-            case _ => {
-                val inputs = line.split(" ").toList
-                if inputs.length < 2 then {
-                    return invalidInput()
-                }
-                val (x, y) = (inputs(0).toIntOption, inputs(1).toIntOption) match {
-                    case (Some(x), Some(y)) => (x-1, y-1)
-                    case _ => return invalidInput()
-                }
-                println(s"Selected ($x, $y)")
-                try {
-                    controller.reveal(x, y)
-                } catch {
-                    case e: IndexOutOfBoundsException => return invalidInput(e.getMessage)
-                }
-                println(controller.field)
-            }
-        }
-        true
-    }
+	def processLine(line: String): Boolean = {
+		line match {
+			case "q" => return false
+			case _ => {
+				val inputs = line.split(" ").toList
+				if inputs.length < 2 then {
+				    return invalidInput()
+				}
+				val (x, y) = (inputs(0).toIntOption, inputs(1).toIntOption) match {
+				    case (Some(x), Some(y)) => (x-1, y-1)
+				    case _ => return invalidInput()
+				}
+				println(s"Selected ($x, $y)")
+				try {
+				    controller.reveal(x, y)
+				} catch {
+				    case e: IndexOutOfBoundsException => return invalidInput(e.getMessage)
+				}
+				println(controller.field)
+				if controller.field.matrix(y)(x).isBomb then {
+					println("You lost!")
+					return false
+				}
+				if controller.field.hasWon then {
+					println("You won!")
+					return false
+				}
+			}
+		}
+		true
+	}
 }
--- src/test/scala/minesweeper/view/TuiSpec.scala
+++ src/test/scala/minesweeper/view/TuiSpec.scala
@@ -8,51 +8,63 @@ import org.scalatest.wordspec.AnyWordSpec
 import java.lang.IndexOutOfBoundsException
 
 class TuiSpec extends AnyWordSpec {
-    "A Tui" when {
-        "it has a single cell field" should {
-            val field = Field(1, 1, (x, y) => Cell(false, false))
-            val controller = FieldController(field)
-            val tui = Tui(controller)
+	"A Tui" when {
+		"it has a single cell field" should {
+			val field = Field(1, 1, (x, y) => Cell(false, false))
+			val controller = FieldController(field)
+			val tui = Tui(controller)
 
-            "without revealing the cell" in {
-                controller.field.toString shouldEqual("#")
-                tui.processLine("abc 2") shouldEqual(true)
-                controller.field.toString shouldEqual("#")
-                tui.processLine("2") shouldEqual(true)
-                controller.field.toString shouldEqual("#")
-            }
-            "after revealing the cell" in {
-                tui.processLine("1 1") shouldEqual(true)
-                controller.field.toString shouldEqual("☐")
-                tui.processLine("2 2") shouldEqual(true)
-                controller.field.toString shouldEqual("☐")
-            }
-            "after quitting" in {
-                tui.processLine("q") shouldEqual(false)
-            }
-        }
-        "it has a multi cell field" should {
-            val field = Field(3, 3, (x, y) => Cell(false, false))
-            val controller = FieldController(field)
-            val tui = Tui(controller)
-            "without revealing the cell" in {
-                controller.field.toString shouldEqual("# # #\n# # #\n# # #")
-                tui.processLine("abc 2") shouldEqual(true)
-                controller.field.toString shouldEqual("# # #\n# # #\n# # #")
-                tui.processLine("abc") shouldEqual(true)
-                controller.field.toString shouldEqual("# # #\n# # #\n# # #")
-            }
-            "after revealing some cells" in {
-                tui.processLine("1 1") shouldEqual(true)
-                controller.field.toString shouldBe("☐ # #\n# # #\n# # #")
-                tui.processLine("3 3") shouldEqual(true)
-                controller.field.toString shouldBe("☐ # #\n# # #\n# # ☐")
-                tui.processLine("4 4") shouldEqual(true)
-                controller.field.toString shouldBe("☐ # #\n# # #\n# # ☐")
-            }
-            "after quitting" in {
-                tui.processLine("q") shouldEqual(false)
-            }
-        }
-    }
+			"without revealing the cell" in {
+				controller.field.toString shouldEqual("#")
+				tui.processLine("abc 2") shouldEqual(true)
+				controller.field.toString shouldEqual("#")
+				tui.processLine("2") shouldEqual(true)
+				controller.field.toString shouldEqual("#")
+			}
+			"after revealing the cell" in {
+				tui.processLine("1 1") shouldEqual(false)
+				controller.field.toString shouldEqual("☐")
+				controller.field.hasWon shouldBe(true)
+				tui.processLine("2 2") shouldEqual(true)
+				controller.field.toString shouldEqual("☐")
+			}
+			"after quitting" in {
+				tui.processLine("q") shouldEqual(false)
+			}
+		}
+		"it has a multi cell field" should {
+			val field = Field(3, 3, (x, y) => Cell(false, false))
+			val controller = FieldController(field)
+			val tui = Tui(controller)
+			"without revealing the cell" in {
+				controller.field.toString shouldEqual("# # #\n# # #\n# # #")
+				tui.processLine("abc 2") shouldEqual(true)
+				controller.field.toString shouldEqual("# # #\n# # #\n# # #")
+				tui.processLine("abc") shouldEqual(true)
+				controller.field.toString shouldEqual("# # #\n# # #\n# # #")
+			}
+			"after revealing some cells" in {
+				tui.processLine("1 1") shouldEqual(true)
+				controller.field.toString shouldBe("☐ # #\n# # #\n# # #")
+				tui.processLine("3 3") shouldEqual(true)
+				controller.field.toString shouldBe("☐ # #\n# # #\n# # ☐")
+				tui.processLine("4 4") shouldEqual(true)
+				controller.field.toString shouldBe("☐ # #\n# # #\n# # ☐")
+				controller.field.hasWon shouldBe(false)
+			}
+			"after quitting" in {
+				tui.processLine("q") shouldEqual(false)
+			}
+		}
+		"it has a field with bombs" should {
+			val field = Field(1, 1, (x, y) => Cell(false, true))
+			val controller = FieldController(field)
+			val tui = Tui(controller)
+			"return false if the game is lost" in {
+				tui.processLine("1 1") shouldEqual(false)
+				controller.field.toString shouldEqual("☒")
+				controller.field.hasWon shouldBe(true)
+			}
+		}
+	}
 }
